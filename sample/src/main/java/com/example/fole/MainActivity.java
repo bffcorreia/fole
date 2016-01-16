6c884cebcba7ca24598fa57bcf61f5b7c9bc3b74
Bruno Correia <bffcorreia@gmail.com>
2016-01-16 17:01:19 +0000

    Sample of Fole library.

--- sample/src/main/java/com/example/fole/MainActivity.java
+++ sample/src/main/java/com/example/fole/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.fole;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import io.github.bffcorreia.fole.Fole;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView textView = (TextView) findViewById(R.id.text_view);
+    final TextView showMore = (TextView) findViewById(R.id.show_more);
+
+    String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+        + "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+        + "exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+        + "irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla "
+        + "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia "
+        + "deserunt mollit anim id est laborum.";
+
+    Fole.with(textView).text(text).maxLines(2).toggle(showMore);
   }
 }
