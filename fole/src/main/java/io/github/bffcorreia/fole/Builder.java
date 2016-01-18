f49687919f0a3f9b8cab9be038a97356e821b64e
Bruno Correia <bffcorreia@gmail.com>
2016-01-18 01:21:06 +0000

    Remove log

--- fole/src/main/java/io/github/bffcorreia/fole/Builder.java
+++ fole/src/main/java/io/github/bffcorreia/fole/Builder.java
@@ -1,6 +1,5 @@
 package io.github.bffcorreia.fole;
 
-import android.util.Log;
 import android.widget.TextView;
 
 public class Builder {
@@ -8,7 +7,6 @@ public class Builder {
   private final TextView textView;
 
   public Builder(TextView textView) {
-    Log.i("MY_DEBUG", "builder textView: " + textView);
     Preconditions.checkArgument(textView != null, "TextView must not be bull.");
     this.textView = textView;
   }
